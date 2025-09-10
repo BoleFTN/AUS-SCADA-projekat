4ed093cf09021c68b8e363736485f00a02b7b566
Nemanja <Nemanja@DESKTOP-SEMRPKP>
2025-09-10 18:23:38 +0200

    EGU i alarmi

--- ProcessingModule/AlarmProcessor.cs
+++ ProcessingModule/AlarmProcessor.cs
@@ -15,7 +15,21 @@ namespace ProcessingModule
         /// <returns>The alarm indication.</returns>
 		public AlarmType GetAlarmForAnalogPoint(double eguValue, IConfigItem configItem)
 		{
-			return AlarmType.NO_ALARM;
+            if (eguValue < configItem.EGU_Min || eguValue > configItem.EGU_Max) { 
+                return AlarmType.REASONABILITY_FAILURE;
+            }
+
+            if (eguValue < configItem.LowLimit)
+            {
+                return AlarmType.LOW_ALARM;
+            }
+
+            if (eguValue > configItem.HighLimit)
+            {
+                return AlarmType.HIGH_ALARM;
+            }
+
+            return AlarmType.NO_ALARM;
 		}
 
         /// <summary>
@@ -26,6 +40,10 @@ namespace ProcessingModule
         /// <returns>The alarm indication.</returns>
 		public AlarmType GetAlarmForDigitalPoint(ushort state, IConfigItem configItem)
 		{
+            if (state == configItem.AbnormalValue)
+            {
+                return AlarmType.REASONABILITY_FAILURE;
+            }
             return AlarmType.NO_ALARM;
         }
 	}
